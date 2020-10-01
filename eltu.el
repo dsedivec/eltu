771d4a6bc2e0fb803032bf51c80582fc94cbbacb
Dale Sedivec <dsedivec@emphasys-software.com>
2020-10-01 14:05:00 -0500

    Version bump for recent changes

--- eltu.el
+++ eltu.el
@@ -4,7 +4,7 @@
 ;;
 ;; Author: Dale Sedivec <dale@codefu.org>
 ;; Keywords: tools
-;; Version: 0.6
+;; Version: 0.7
 ;; Package-Requires: ((emacs "25.1"))
 ;;
 ;; This program is free software; you can redistribute it and/or modify
